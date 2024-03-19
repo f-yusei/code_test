faefd7055cbfe47c17a062fee858bc670642d98b
f-yusei <fkys2932@gmail.com>
2024-03-19 14:50:57 +0900

    feat: create haribote

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,48 @@
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h2>Title</h2>
+      <p>都道府県を選択してください</p>
+      <form>
+        <label>
+          <input type="checkbox" name="tokyo" value="tokyo" />
+          東京
+        </label>
+        <label>
+          <input type="checkbox" name="kanagawa" value="kanagawa" />
+          神奈川
+        </label>
+        <label>
+          <input type="checkbox" name="saitama" value="saitama" />
+          埼玉
+        </label>
+        <label>
+          <input type="checkbox" name="chiba" value="chiba" />
+          千葉
+        </label>
+        <label>
+          <input type="checkbox" name="ibaraki" value="ibaraki" />
+          茨城
+        </label>
+        <label>
+          <input type="checkbox" name="tochigi" value="tochigi" />
+          栃木
+        </label>
+        <label>
+          <input type="checkbox" name="gunma" value="gunma" />
+          群馬
+        </label>
+      </form>
+      <div>
+        <h2>グラフ</h2>
+        <p>グラフが表示されます</p>
+        <div>
+          <button>総人口</button>
+          <button>年少人口</button>
+          <button>生産年齢人口</button>
+          <button>老年人口</button>
+        </div>
+      </div>
     </main>
   );
 }
